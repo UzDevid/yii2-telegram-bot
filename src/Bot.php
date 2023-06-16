8b8355d5d77a547cf1ecb498ddd89792fc231e32
uzdevid <uzdevid@gmail.com>
2023-06-16 15:59:35 +0500

    Доработки

--- src/Bot.php
+++ src/Bot.php
@@ -5,6 +5,8 @@ namespace uzdevid\telegram\bot;
 use uzdevid\telegram\bot\handler\Handler;
 use uzdevid\telegram\bot\inline\Inliner;
 use uzdevid\telegram\bot\inline\InlinerInterface;
+use uzdevid\telegram\bot\message\Editor;
+use uzdevid\telegram\bot\message\EditorInterface;
 use uzdevid\telegram\bot\message\Sender;
 use uzdevid\telegram\bot\message\SenderInterface;
 use yii\base\InvalidCallException;
@@ -37,6 +39,28 @@ class Bot extends BaseBot {
         }
 
         return $sender;
+    }
+
+    public function editor(EditorInterface|null $editor = null): Editor {
+        if ($editor === null) {
+            $editor = new Editor();
+        }
+
+        if (!isset($this->token)) {
+            throw new InvalidCallException('Token is not set');
+        }
+
+        $editor->setToken($this->token);
+
+        if (isset($this->chatId)) {
+            $editor->setChatId($this->chatId);
+        }
+
+        if (isset($this->username)) {
+            $editor->setUsername($this->username);
+        }
+
+        return $editor;
     }
 
     public function inliner(InlinerInterface|null $inliner = null): Inliner {
--- src/message/Editor.php
+++ src/message/Editor.php
@@ -2,6 +2,37 @@
 
 namespace uzdevid\telegram\bot\message;
 
-class Editor extends Message implements EditorInterface{
+class Editor extends Message implements EditorInterface {
+    private int|null $_messageId = null;
 
+    public function methodUrl(string|null $method = null): string {
+        return self::$apiUrl . $this->token . '/' . $method ?? $this->message->methodName();
+    }
+
+    public function getMessageId() {
+        return $this->_messageId;
+    }
+
+    public function setMessageId(int $messageId) {
+        $this->_messageId = $messageId;
+    }
+
+    public function messageId(int $messageId) {
+        $this->messageId = $messageId;
+        return $this;
+    }
+
+    public function edit() {
+
+    }
+
+    public function delete() {
+        $query = ['chat_id' => $this->chatIdOrUsername(), 'message_id' => $this->messageId];
+
+        $options = ['query' => $query];
+
+        print_r($options);
+
+        $response = $this->httpClient->get($this->methodUrl('deleteMessage'), $options);
+    }
 }
