e5b9befb9ed79da04a041c02056748a51f6f42cf
uzdevid <uzdevid@gmail.com>
2024-04-05 14:38:57 +0500

    Добавил конструктор

--- src/Message/Message/Method/SendChatAction.php
+++ src/Message/Message/Method/SendChatAction.php
@@ -6,6 +6,13 @@ use UzDevid\Telegram\Bot\Message\Message\Method;
 use UzDevid\Telegram\Bot\Message\Message\MethodInterface;
 
 class SendChatAction extends Method implements MethodInterface {
+    protected static string $__action = 'action';
+
+    public function __construct(string $action, array $attributes = []) {
+        $this->addAttribute(self::$__action, $action);
+        parent::__construct($attributes);
+    }
+
     /**
      * @return string
      */
